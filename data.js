8c99bdc408e09638376dc46b15e1d1a1e5ccc8f9
radot1530-ai <radot1530@gmail.com>
2025-12-03 18:45:09 -0500

    Add data.js with property listings and services

---
+++ data.js
@@ -0,0 +1,90 @@
+// data.js
+
+const kayList = [
+  {
+    id: 1,
+    tit: "Kay modèn nan Delmas",
+    pri: "$400 / mwa",
+    adresse: "Delmas 75, Pòtoprens",
+    deskripsyon: "3 chanm, 2 twalèt, dlo 24/24...",
+    imaj: "image/Screenshot_20251201-172702.png",
+    adminwhatsapp: "Achte/Negosye"
+  },
+  {
+    id: 2,
+    tit: "Maison à la campagne",
+    pri: "$2000",
+    adresse: ".., Ayiti",
+    deskripsyon: "Plizyè chanm",
+    imaj: "image/Screenshot_20251109-155526.png",
+    adminwhatsapp: "Achte/Negosye"
+  },
+  {
+    id: 3,
+    tit: "Renovasyon kay",
+    pri: "$100 / jou",
+    adresse: "Pòtoprens",
+    deskripsyon: "Sèvis renovasyon kay pwofesyonèl",
+    imaj: "image/service1.jpg",
+    adminwhatsapp: "Achte/Negosye"
+  }]
+   
+const terresList = [
+      {
+    id: 1,
+    tit: "Tè pou lwe nan Camp-Perrin",
+    pri: "$400 / mwa",
+    adresse: " Ayiti/Camp-Perrin",
+    deskripsyon: "2¹/2 ekta...",
+    imaj: "/image/Screenshot_20251109-155633.png",
+    adminwhatsapp: "Achte/Negosye"
+  },
+  {
+    id: 2,
+    tit: "Tè nan Croix-des-Bouquets",
+    pri: "$2000",
+    adresse: "Croix-des-Bouquets, Ayiti",
+    deskripsyon: "1 ekta tè pou konstwiksyon oswa agrikilti",
+    imaj: "image/Screenshot_20251109-155701.png",
+    adminwhatsapp: "Achte/Negosye"
+  },
+  {
+    id: 3,
+    tit: "Renovasyon kay",
+    pri: "$100 / jou",
+    adresse: "Pòtoprens",
+    deskripsyon: "Sèvis renovasyon kay pwofesyonèl",
+    imaj: "image/service1.jpg",
+    adminwhatsapp: "Achte/Negosye"
+  }]
+  
+const servicesList = [
+  {
+    id: 1,
+    tit: " Abiman ",
+    pri: " ...",
+    adresse: " Lòt modèl yo poko disponib ",
+    deskripsyon: " Kontakte si w vle antre an kontak ak bòs kouti a...",
+    imaj: "/image/IMG-20251201-WA0009.jpg",
+    adminwhatsapp: "Achte/Negosye"
+  },
+  {
+    id: 2,
+    tit: "Kamera Profesyonèl",
+    pri: " ... ",
+    adresse: "... , Ayiti",
+    deskripsyon: " ... ",
+    imaj: "/image/Screenshot_20251203-182013.png",
+    adminwhatsapp: "Achte/Negosye"
+  },
+  {
+    id: 3,
+    tit: "Renovasyon kay",
+    pri: "$100 / jou",
+    adresse: "Pòtoprens",
+    deskripsyon: "Sèvis renovasyon kay pwofesyonèl",
+    imaj: "",
+    adminwhatsapp:"Achte/Negosye"
+  }
+
+];
